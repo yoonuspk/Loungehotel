c44f1a18eb2aed3dd6c088d91d5455b38cccdb87
Yoonus <pkyoonus09@gmail.com>
2018-10-28 19:58:57 -0400

    Dialog added]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { UserService } from './services/user.service';
 
 import {CalendarModule} from 'primeng/calendar';
 import { MyDatePickerModule } from 'mydatepicker';
+import { BooknowComponent } from './booknow/booknow.component';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { MyDatePickerModule } from 'mydatepicker';
     AboutComponent,
     GalleryComponent,
     RoomsComponent,
-    ContactusComponent
+    ContactusComponent,
+    BooknowComponent
   ],
   imports: [
     AppRoutingModule,
@@ -52,7 +54,7 @@ import { MyDatePickerModule } from 'mydatepicker';
 
   ],
   providers: [RoomsService,CategoryService,BookingService,UserService],
-  entryComponents:[BooklistComponent],
+  entryComponents:[BooklistComponent,BooknowComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -3,6 +3,8 @@ import { Room } from "../domains/rooms";
 import { Category, SubCategory } from "../domains/category";
 import { RoomsService } from "../services/rooms.service";
 import { CategoryService } from "../services/category.service";
+import { DialogService } from "ng2-bootstrap-modal";
+import { BooknowComponent } from "../booknow/booknow.component";
 
 @Component({
   selector: "app-rooms",
@@ -11,31 +13,41 @@ import { CategoryService } from "../services/category.service";
 })
 
 export class RoomsComponent implements OnInit {
-  
+
   rooms: Room[];
   categories: Category[];
   subcategories: SubCategory[];
-  constructor(private roomSvc : RoomsService,
-     private catSvc : CategoryService) { 
+  constructor(private roomSvc: RoomsService,
+    private catSvc: CategoryService, private _dialogSvc: DialogService) {
 
   }
 
   ngOnInit() {
 
-    this.catSvc.getAllCategories().subscribe(x=>{this.categories=x;console.log(this.categories)})
-    this.catSvc.getAllSubCategories().subscribe(x=>{this.subcategories=x;console.log(this.subcategories)})
-    this.roomSvc.getAllRooms().subscribe(x=>{this.rooms=x;console.log(this.rooms)})
+    this.catSvc.getAllCategories().subscribe(x => { this.categories = x; console.log(this.categories) })
+    this.catSvc.getAllSubCategories().subscribe(x => { this.subcategories = x; console.log(this.subcategories) })
+    this.roomSvc.getAllRooms().subscribe(x => { this.rooms = x; console.log(this.rooms) })
   }
 
-  getCatName(id, cat){
-    return cat[id-1].CatName;
-  }
-  
-  getSubCatName(id, cat){
-    return cat[id-1].SCatName;
+  getCatName(id, cat) {
+    return cat[id - 1].CatName;
   }
 
-  booknow(){
-    
+  getSubCatName(id, cat) {
+    return cat[id - 1].SCatName;
+  }
+
+  booknow() {
+    let disposable = this._dialogSvc
+      .addDialog(BooknowComponent, {})
+      .subscribe((editedIncome) => {
+        if (editedIncome) {
+          console.log(editedIncome);
+          // let i = this.inc.indexOf(selectedInc);
+          // this.inc[i]=editedIncome; //sync the changes back to data model
+          // this.populateTable(this.inc); //refresh data table
+        }
+      });
   }
 }
+
